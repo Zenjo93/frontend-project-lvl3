54cf940a42247af98804327341a8736da61b1cc2
Zenjo <vl.brzn@gmail.com>
2021-08-30 17:17:36 +0300

    add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-    mode: process.env.NODE_ENV || 'development',
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-            {test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader']},
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
-            },
-            {
-                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                use: 'url-loader?limit=10000',
-            },
-            {
-                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-                use: 'file-loader',
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'RSS агрегатор',
-            template: 'index.html',
-        }),
+  mode: process.env.NODE_ENV || 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
     ],
-}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'RSS агрегатор',
+      template: 'index.html',
+    }),
+    new ESLintPlugin({ fix: true }),
+  ],
+};
