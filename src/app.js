3f760c2f6ecffe0522ef2a0dbeb2d5541e02300a
Zenjo <vl.brzn@gmail.com>
2021-09-16 12:48:53 +0300

    fix checkupdates

--- src/app.js
+++ src/app.js
@@ -34,26 +34,14 @@ const addNewPosts = (feeds, posts) => feeds.forEach((feed) => {
     }));
 
     const diff = _.differenceBy(oldPostsNormalize, newPosts, 'title');
+    diff.forEach((item) => item.id = id);
+    console.log(diff);
 
     if (diff.length !== 0) {
       posts.push(diff);
     }
   });
 });
-
-// http://lorem-rss.herokuapp.com/feed?unit=second
-
-// let timerId = setTimeout(function request() {
-// ...отправить запрос...
-//
-//   if (ошибка запроса из-за перегрузки сервера) {
-//     // увеличить интервал для следующего запроса
-//     delay *= 2;
-//   }
-//
-//   timerId = setTimeout(request, delay);
-//
-// }, delay);
 
 export default () => {
   const state = {
@@ -100,28 +88,5 @@ export default () => {
         watchedState.form.valid = false;
         watchedState.form.error = err.message;
       });
-
-    // if (watchedState.init) {
-    //   checkFeedUpdate(watchedState.feeds, watchedState.posts);
-    // }
   });
 };
-
-// feedList - идем по списку урлов, парсим данные, берем только посты
-// сравнить новые список постов со старым по тайтлу
-// если тайтл отличается -> новый пост, добавляем в список постов, присваиваем id фида,
-//
-
-// const feed: {
-//   title:,
-//   description:,
-//   id:,
-// }
-//
-// const posts = [
-//   {
-//     title:,
-//     link:,
-//     postid:,
-//   }
-// ]
